5ace5916687e75a075d3d2d35f02123b55676a98
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-05-29 14:25:39 +0100

    validation done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,22 @@
     <div class="col-md-2"></div>
     <div class="col-md-8">
       <h2 class="text-left text-primary">VueValidation Form</h2>
+      <div
+        v-if="submitStatus == 'OK'"
+        class="alert alert-success alert-dismissible fade show typo__p"
+        role="alert"
+      >
+        <strong>Success!</strong> form submitted successfully
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
       <form @submit.prevent="submit">
         <div class="form-group" :class="{ 'form-group--error': $v.email.$error }">
           <label for="email">Email address</label>
@@ -11,74 +27,122 @@
             class="form-control"
             id="email"
             name="email"
-            v-model="email"
+            v-model.trim="email"
             aria-describedby="emailHelp"
             placeholder="Enter email"
           />
         </div>
-        <p class="text-error" v-if="!$v.email.required">Field is required</p>
+        <p class="text-danger" v-if="!$v.email.required">Email is required</p>
         <p
-          class="text-error"
+          class="text-danger"
           v-if="!$v.email.minLength"
         >Email must have at least {{$v.email.$params.minLength.min}} letters.</p>
 
-        <div class="form-group">
+        <div class="form-group" :class="{ 'form-group--error': $v.firstname.$error }">
           <label for="fname">First Name</label>
           <input
             type="text"
             class="form-control"
             name="firstname"
-            v-model="firstname"
+            v-model.trim="firstname"
             id="fname"
             placeholder="First Name"
           />
         </div>
-        <div class="form-group">
+        <p class="text-danger" v-if="!$v.firstname.required">Firstname is required</p>
+        <p
+          class="text-danger"
+          v-if="!$v.firstname.minLength"
+        >Firstname must have at least {{$v.firstname.$params.minLength.min}} letters.</p>
+
+        <p
+          class="text-danger"
+          v-if="!$v.firstname.maxLength"
+        >Firstname less than 100 {{$v.firstname.$params.maxLength.max}} letters.</p>
+
+        <p class="text-danger" v-if="!$v.firstname.alpha">Firstname must be a letter</p>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.lastname.$error }">
           <label for="lname">Last Name</label>
           <input
             type="text"
             class="form-control"
-            v-model="lastname"
+            v-model.trim="lastname"
             name="lastname"
             id="lname"
             placeholder="Last Name"
           />
         </div>
+        <p class="text-danger" v-if="!$v.lastname.required">lastname is required</p>
+        <p
+          class="text-danger"
+          v-if="!$v.lastname.minLength"
+        >lastname must have at least {{$v.lastname.$params.minLength.min}} letters.</p>
+        <p
+          class="text-danger"
+          v-if="!$v.lastname.maxLength"
+        >lastname must less than 100 {{$v.lastname.$params.maxLength.max}} letters.</p>
+        <p class="text-danger" v-if="!$v.lastname.alpha">lastname must be a letter</p>
 
-        <div class="form-group">
+        <div class="form-group" :class="{ 'form-group--error': $v.username.$error }">
           <label for="lname">Username</label>
           <input
             type="text"
             class="form-control"
-            v-model="username"
+            v-model.trim="username"
             name="username"
             id="username"
-            placeholder="Uername"
+            placeholder="Username"
           />
         </div>
+        <p class="text-danger" v-if="!$v.username.required">Username is required</p>
+        <p
+          class="text-danger"
+          v-if="!$v.username.minLength"
+        >Username must have at least {{$v.username.$params.minLength.min}} letters.</p>
+        <p
+          class="text-danger"
+          v-if="!$v.username.maxLength"
+        >Username must have at least {{$v.username.$params.maxLength.max}} letters.</p>
+        <p class="text-danger" v-if="!$v.username.alpha">Username must be a letter</p>
 
-        <div class="form-group">
+        <div class="form-group" :class="{ 'form-group--error': $v.password.$error }">
           <label for="password">Password</label>
           <input
             type="password"
             name="password"
-            v-model="password"
+            v-model.trim="password"
             class="form-control"
             id="password"
             placeholder="Password"
           />
         </div>
-        <div class="form-group">
+        <p class="text-danger" v-if="!$v.password.required">password is required</p>
+        <p
+          class="text-danger"
+          v-if="!$v.password.minLength"
+        >password must have at least {{$v.password.$params.minLength.min}} letters.</p>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.comfirm_password.$error }">
           <label for="Cpassword">Confirm Password</label>
           <input
             type="password"
             name="Cpassword"
             class="form-control"
-            v-model="comfirm_password"
+            v-model.trim="comfirm_password"
             id="Cpassword"
             placeholder="Confirm Password"
           />
         </div>
+        <p class="text-danger" v-if="!$v.comfirm_password.required">Confirmation field is required</p>
+        <!-- <p
+          class="text-danger"
+          v-if="!$v.comfirm_password.minLength"
+        >commfirmation field must have at least {{$v.comfirm_password.$params.minLength.min}} letters.</p>-->
+        <p
+          class="text-danger"
+          v-if="!$v.comfirm_password.sameAsPassword"
+        >Passwords must be identical {{$v.comfirm_password.sameAs }} letters.</p>
 
         <button
           type="submit"
@@ -88,9 +152,6 @@
           class="btn btn-primary"
         >Submit</button>
         <button type="reset" name="reset" id="reset" class="btn btn-danger ml-2">Reset</button>
-        <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your submission!</p>
-        <p class="typo__p" v-if="submitStatus === 'ERROR'">Please fill the form correctly.</p>
-        <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
       </form>
     </div>
     <div class="col-md-2"></div>
@@ -104,10 +165,13 @@ const {
   maxLength,
   sameAs
 } = require("vuelidate/lib/validators");
+import { helpers } from "vuelidate/lib/validators";
+const alpha = helpers.regex("alpha", /^[a-zA-Z]*$/);
+
 export default {
   data() {
     return {
-       submitStatus: null,
+      submitStatus: null,
       email: "",
       firstname: "",
       lastname: "",
@@ -120,17 +184,20 @@ export default {
     firstname: {
       required,
       minLength: minLength(4),
-      maxLength: maxLength(100)
+      maxLength: maxLength(100),
+      alpha
     },
     lastname: {
       required,
       minLength: minLength(4),
-      maxLength: maxLength(100)
+      maxLength: maxLength(100),
+      alpha
     },
     username: {
       required,
       minLength: minLength(4),
-      maxLength: maxLength(100)
+      maxLength: maxLength(100),
+      alpha
     },
     email: {
       required,
@@ -140,23 +207,18 @@ export default {
     password: {
       required,
       minLength: minLength(4),
-      maxLength: maxLength(100),
-      sameAs: sameAs(function() {
-        return this.password2;
-      })
-      // sameAs: sameAs(this.comfirm_password)
+      maxLength: maxLength(100)
     },
     comfirm_password: {
       required,
       minLength: minLength(4),
-      maxLength: maxLength(100)
+      maxLength: maxLength(100),
+      sameAsPassword: sameAs(function() {
+        return this.password;
+      })
     }
   },
   methods: {
-    // setEmail(value) {
-    //   this.email = value;
-    //   this.$v.email.$touch();
-    // }
     submit() {
       console.log("submit!");
       this.$v.$touch();
@@ -164,6 +226,13 @@ export default {
         this.submitStatus = "ERROR";
       } else {
         // do your submit logic here
+        console.log(
+          this.firstname +
+            this.lastname +
+            this.email +
+            this.password +
+            this.username
+        );
         this.submitStatus = "PENDING";
         setTimeout(() => {
           this.submitStatus = "OK";
